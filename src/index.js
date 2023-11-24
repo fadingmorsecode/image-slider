6206bd481b393c8faf8b63ddd406a7e88e28e19d
Drake Williamson <williamson.drake@outlook.com>
2023-11-24 14:35:03 -0800

    Import images

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import arrowImage from "./assets/arrow.svg";
 import { forwardSlide, reverseSlide, slideInitialization } from "./slide";
 import loadArrowListeners from "./arrowlisteners";
 import loadDotListeners from "./dotlistener";
+import pic from "./assets/pic.jpg";
+import pic1 from "./assets/pic1.jpg";
+import pic2 from "./assets/pic2.jpg";
+import pic3 from "./assets/pic3.jpg";
+import pic4 from "./assets/pic4.jpg";
 
 const arrowLeftContainer = document.querySelector(".arrow-container-left");
 const arrowRightContainer = document.querySelector(".arrow-container-right");
@@ -17,6 +22,23 @@ arrowIconRight.src = arrowImage;
 
 arrowLeftContainer.append(arrowIconLeft);
 arrowRightContainer.append(arrowIconRight);
+
+const imageOne = new Image();
+imageOne.src = pic;
+const imageTwo = new Image();
+imageTwo.src = pic1;
+const imageThree = new Image();
+imageThree.src = pic2;
+const imageFour = new Image();
+imageFour.src = pic3;
+const imageFive = new Image();
+imageFive.src = pic4;
+
+document.querySelector(".one").appendChild(imageOne);
+document.querySelector(".two").appendChild(imageTwo);
+document.querySelector(".three").appendChild(imageThree);
+document.querySelector(".four").appendChild(imageFour);
+document.querySelector(".five").appendChild(imageFive);
 
 slideInitialization();
 loadArrowListeners();
